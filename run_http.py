c3869f0cab1dbf6237366de119700ddd12aac8ea
Namora Digital <148518287+namoradigital@users.noreply.github.com>
2024-07-08 21:45:08 +0700

    Update run_http.py

--- run_http.py
+++ run_http.py
@@ -78,7 +78,7 @@ async def main():
     #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
     _user_id = input('Please Enter your user ID: ')
     #put the proxy in a file in the format socks5://username:password@ip:port or socks5://ip:port
-    r = requests.get("https://github.com/monosans/proxy-list/raw/main/proxies/http.txt", stream=True)
+    r = requests.get("https://raw.githubusercontent.com/namoradigital/Premium-proxy/main/http.txt", stream=True)
     if r.status_code == 200:
         with open('http.txt', 'wb') as f:
             for chunk in r:
